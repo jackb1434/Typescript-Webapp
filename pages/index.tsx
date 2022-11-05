91ca505208191b89681b9a001de92da76422a9cc
jackb1434 <jackbuckle34@gmail.com>
2022-11-04 20:23:33 -0500

    fixed null error (hopefully)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { changeValue } from './main'
+import { changeValue } from '../scripts/main'
 
 const Home: NextPage = () => {
   return (
