4df1db5c5d4014e6aa4a5d03de4bb8a8209a78d3
jackb1434 <jackbuckle34@gmail.com>
2022-11-04 20:15:11 -0500

    fixed null issues

--- pages/main.tsx
+++ pages/main.tsx
@@ -12,31 +12,47 @@ let words: string[] = [
     "Buy my beats"
 ]
 
-
 var index: number;
-var lastNum: number = words.length + 1; 
+var lastNum: number = words.length + 1;
+
+var canUse: boolean = false;
 
 
 export function changeValue(){
-
-    index = Math.floor(Math.random() * words.length);
+    if (canUse) {
+        var el = document.getElementById("para") as HTMLElement;
+        index = Math.floor(Math.random() * words.length);
     
-    
-    // Makes sure it will not print same string twice or more
-    if (index == lastNum){
-        console.log("Same index");
+        // Makes sure it will not print same string twice or more
+        if (index == lastNum){
+            console.log("Same index");
 
-        while (index == lastNum){
-            index = Math.floor(Math.random() * words.length);
-            if (index != lastNum){
-                document.getElementById("para").innerHTML = words[index];
-                break;
+            while (index == lastNum){
+                index = Math.floor(Math.random() * words.length);
+                if (index != lastNum){
+                    el.innerHTML = words[index];
+                    break;
+                }
             }
+        lastNum = index;
         }
-        lastNum = index;
+        else{
+            el.innerHTML = words[index];
+            lastNum = index;
+        }
+    }else{
+        console.log("document not initialized...")
+        return;
     }
-    else{
-        document.getElementById("para").innerHTML = words[index];
-        lastNum = index;
+    
+}
+
+function init(){
+    if (typeof window === "object"){
+        document.addEventListener("DOMContentLoaded", function(){
+            canUse = true;
+        })
     }
 }
+
+init();
